db742cb50b400b00d46c2e1dee7c7c277261b24f
IPRIT <ipritoflex@yandex.ru>
2018-03-16 15:13:40 +0300

    Bump version 1.12.0

--- lib/sass-bem.rb
+++ lib/sass-bem.rb
@@ -3,6 +3,6 @@ extension_path = File.expand_path(File.join(File.dirname(__FILE__), ".."))
 Compass::Frameworks.register('sass-bem', :path => extension_path)
 
 module SassBem
-  VERSION = "1.11.0"
+  VERSION = "1.12.0"
   DATE = "2016-02-07"
 end
